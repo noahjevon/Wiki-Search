e1d6ff8cc6b5c14273c67100c419208173dab81b
Noah <noahjevon@gmail.com>
2021-05-18 17:40:23 +0100

    Initial Commit

---
+++ js/js.js
@@ -0,0 +1,29 @@
+var selected_text;
+
+chrome.tabs.executeScript( {
+    code: "window.getSelection().toString();"
+}, function(selection) {
+    document.getElementById("selected").innerHTML = "Selected Text: " + selection[0];
+    selected_text = selection[0];
+});
+
+document.getElementById("button").addEventListener("click", searchWiki);
+
+// function searchWiki() {
+//         fetch("https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=" + selected_text).then(function(resp) {
+//         return resp.json()
+//     }).then(function(data) {
+//     document.getElementById("contents").innerHTML = (JSON.stringify(data.extract));
+//     })
+// }
+
+function searchWiki() {
+    const searchWiki = async search => {
+        const response = await fetch(`https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=${search}`)
+        return await response.json();
+      }
+      
+      searchWiki(selected_text).then(data => {
+        document.getElementById("contents").innerHTML = (JSON.stringify(Object.values(data.query.pages)[0].extract));
+      });
+}
